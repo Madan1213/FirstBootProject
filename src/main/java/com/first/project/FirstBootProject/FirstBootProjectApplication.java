878aec8a89bef4a86e527c836fef74ea4bb70d0f
Personal PC <madangowda1213@gmail.com>
2024-04-26 11:19:03 +0530

    Initial commit

---
+++ src/main/java/com/first/project/FirstBootProject/FirstBootProjectApplication.java
@@ -0,0 +1,14 @@
+package com.first.project.FirstBootProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBootProjectApplication {
+
+	public static void main(String[] args)
+	{
+		SpringApplication.run(FirstBootProjectApplication.class, args);
+	}
+
+}
