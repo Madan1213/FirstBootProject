f06d4416276ca18aadd44c63c40e591aaef370ff
Personal PC <madangowda1213@gmail.com>
2024-04-30 00:58:24 +0530

    Some extra code

--- src/main/java/com/first/project/FirstBootProject/configuration/MyConfig.java
+++ src/main/java/com/first/project/FirstBootProject/configuration/MyConfig.java
@@ -2,6 +2,7 @@ package com.first.project.FirstBootProject.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -15,31 +16,33 @@ import org.springframework.security.web.SecurityFilterChain;
 import javax.sql.DataSource;
 
 @Configuration
-@EnableWebSecurity
 public class MyConfig
 {
 
     @Bean
     public UserDetailsService userDetailsService(DataSource dataSource)
     {
+        System.out.println("Creating UserDetailsService bean...");
+
         JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
 
-        //Define Query to retrieve user by username
         jdbcUserDetailsManager.setUsersByUsernameQuery(
-                "select email,password,enabled from user where id=?"
+                "select email,password,enabled from user where email=?"
         );
-        //Define query to retrieve the authorities/roles by username
+
         jdbcUserDetailsManager.setAuthoritiesByUsernameQuery(
-                "select role from user where id=?"
+                "select role from user where email=?"
         );
+
+        System.out.println("UserDetailsService bean created successfully.");
         return jdbcUserDetailsManager;
     }
 
-    @Bean
+   /* @Bean
     public BCryptPasswordEncoder passwordEncoder()
     {
         return new BCryptPasswordEncoder();
-    }
+    }*/
 
 
     /*@Bean
@@ -49,17 +52,28 @@ public class MyConfig
     }*/
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeHttpRequests(configurer->
+        http.
+                authorizeHttpRequests(configurer->
                 configurer.requestMatchers("/admin/**").hasRole("ADMIN")
-                        .requestMatchers("/user/**").hasRole("USER")
-                        .anyRequest().authenticated()
-        ).formLogin(form->
-                form.loginPage("/showLoginForm")
-                        .loginProcessingUrl("/authenticateTheUser")
-                        .permitAll()
-                ).logout(logout->
-                    logout.permitAll()
+                .requestMatchers("/user/**").hasRole("USER")
+                .requestMatchers("/","/signup","about").permitAll()
+                .anyRequest().authenticated()
+                ).formLogin(form ->
+                        form.loginPage("/showLoginForm")
+                                .usernameParameter("email")
+                                .loginProcessingUrl("/authenticateTheUser")
+                                .permitAll()
+                )
+                .logout(logout ->
+                        logout.permitAll().logoutSuccessUrl("/")
+                )
+                .exceptionHandling(configurer->
+                        configurer.accessDeniedPage("/access-denied")
                 );
+
         return http.build();
     }
+
+
+
 }
